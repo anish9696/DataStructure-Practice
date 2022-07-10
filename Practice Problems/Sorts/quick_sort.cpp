302be664817761a60ef59e9085104d71175e3f22
Anish Kumar <anish.9696@gmail.com>
2022-07-10 17:41:21 +0530

    adding merge sort and quick sort

--- Practice Problems/Sorts/quick_sort.cpp
+++ Practice Problems/Sorts/quick_sort.cpp
@@ -1,36 +1,52 @@
-//insertion sort works as one part is sorted and another part is unsorted 
-// takes one element and put it in correct position and then move 
-// array = {12, 11, 13, 5, 6};
+// quick sort using divide and conquer approach it's worst order complexity is o(n2)and best and average order case complexity is o(nlgn)
+
 
 #include<iostream>
 using namespace std;
 
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 void print_array(int arr[],int size){
-    for(int i = 0;i<siz;i++){
+    for(int i = 0;i<size;i++){
         cout<<arr[i]<<" ";
     }
     cout <<endl;
 }
 
-void insertion_sort(int arr[], int size){
-    int i,key, j;
-    for( i =1;i<size;i++) {
-        key = arr[i];
-        j = i -1;
-        while(j >= 0 && arr[j]> key){
-            arr[j+1] = arr[j];
-            j = j-1;
+int partition(int array[],int low, int high) {
+
+    int pivot = array[high];
+    int i = (low -1);
+
+    for (int j = low;j<= high; j++) {
+        if (array[j] < pivot) {
+            i++;
+            swap(&array[i], &array[j]);
         }
-        arr[j+1] = key;
+    }
+    swap(&array[i+1], &array[high]);
+    return (i+1);
+}
+
+void quick_sort(int array[],int low , int high) {
+    if( low < high) {
+
+        int pi = partition(array, low, high);
+
+        quick_sort(array, low, pi-1);
+        quick_sort(array, pi+1, high);
     }
 }
 
-
 int main(){
 
-    int arr[] = {12, 11, 13, 5, 6};
+    int arr[] = {64, 25, 12, 22, 11};
     int size = sizeof(arr)/sizeof(int);
-    insertion_sort(arr, size);
+    quick_sort(arr, 0, size-1);
     print_array(arr, size);
     return 0;
 }
